a3b7f3eac6fe3f09149a2473b3fb2adf1e7ef81d
Jennifer <jenniferlewmunoz@gmail.com>
2022-04-07 09:44:10 -0700

    Code works with new assets
    2 avatars, 4 grabbable food items

--- sketch.js
+++ sketch.js
@@ -4,14 +4,14 @@
   Use of the p5.play library with a sprite class
   
 ------------------------------------------------------------------------------------
-	To use:
-	Add this line to the index.html
+  To use:
+  Add this line to the index.html
 
   <script src="p5.timer.js"></script>
 ***********************************************************************************/
 
 // This is a speed of the 'sprite' which we can move
-var speed = 20;
+var speed = 30;
 
 // an array of all the class avaatars
 var playerAvatar;
@@ -37,25 +37,30 @@ var preventRepickup = false;      // two objects
 var opened = false;
 
 function preload() {
- 
+
   // Add new avatar animations here
   playerAvatar = new Avatar("Player", 100, 150);
-   
+
   // MODIFY THIS: to make your avatar go faster or slower
   playerAvatar.setMaxSpeed(5);
 
-  door = new StaticSprite("Door", 900,700, 'assets/door.png');
+  door = new StaticSprite("Door", 900, 700, 'assets/door.png');
 
   // default direction facing LEFT for the moving animation
   // MODIFY THIS: change to your avatar filenames
-  playerAvatar.addMovingAnimation( 'assets/run1.png', 'assets/run2.png');
-  playerAvatar.addStandingAnimation('assets/standing1.png', 'assets/standing2.png');
+  playerAvatar.addMovingAnimation('assets/female_running01.png', 'assets/female_running03.png');
+  playerAvatar.addStandingAnimation('assets/female_standing01.png', 'assets/female_standing02.png');
 
   // MODIFY THIS - add more grabbables beloiw
   // Add grabbables - these appear on top of the player icon
-  grabbables.push(new StaticSprite("Star", 500,500, 'assets/fullStar.png'));
-  grabbables.push(new StaticSprite("Key", 750,200, 'assets/key.png'));
-  grabbables.push(new StaticSprite("Wheel", 400,300, 'assets/wheel.png'));
+  grabbables.push(new StaticSprite("Star", 500, 500, 'assets/fullStar.png'));
+  grabbables.push(new StaticSprite("Key", 750, 200, 'assets/key.png'));
+  grabbables.push(new StaticSprite("Wheel", 400, 300, 'assets/wheel.png'));
+
+  grabbables.push(new StaticSprite("Apple", 100, 600, 'assets/apple.png'));
+  grabbables.push(new StaticSprite("Banana", 200, 600, 'assets/banana.png'));
+  grabbables.push(new StaticSprite("Steak", 300, 600, 'assets/steak.png'));
+  grabbables.push(new StaticSprite("Bacon", 400, 600, 'assets/bacon.png'));
 }
 
 // Setup code goes here
@@ -64,19 +69,19 @@ function setup() {
   frameRate(30);
 
   // This will setup the animation
-  for( let i = 0; i < grabbables.length; i++ ) {
+  for (let i = 0; i < grabbables.length; i++) {
     grabbables[i].setup();
   }
 
   // loads image for the door
   door.setup();
- }
+}
 
 // Draw code goes here
 function draw() {
   // MODIFY THIS: if you want to do something more with the key
-  if( opened ) {
-    background(128,80,0);
+  if (opened) {
+    background(128, 80, 0);
   }
   else {
     background(128);
@@ -98,27 +103,27 @@ function checkOverlaps() {
   // We are looking for an overlap of the player avatar with the door.
   // the 2nd argument is a local callback function, that will get called repeatedly while
   // we may build better ov
- playerAvatar.sprite.overlap(door.sprite, doorCollision);
+  playerAvatar.sprite.overlap(door.sprite, doorCollision);
 
   // go through grabble array and set these 
   overlapCount = 0;
-  for( let i = 0; i < grabbables.length; i++ ) {
+  for (let i = 0; i < grabbables.length; i++) {
     playerAvatar.sprite.overlap(grabbables[i].sprite, grabbableCollision);
   }
 
   // we aren't overlapping, so prevent re-pickup
-  if( overlapCount === 0 ) {
+  if (overlapCount === 0) {
     preventPickup = false;
   }
-  if( overlapCount === 1 ) {
+  if (overlapCount === 1) {
     preventRepickup = false;
   }
 }
 
 // This will reset position
 function keyPressed() {
-  if( key === ' ') {
-    if( playerAvatar.grabbable !== undefined ) {
+  if (key === ' ') {
+    if (playerAvatar.grabbable !== undefined) {
       preventPickup = true;
       playerAvatar.clearGrabbable();
     }
@@ -132,17 +137,13 @@ function keyPressed() {
 // MODIFY THIS: if you are using multiple avatars for selection. Change the filenames
 function checkSelectAvatar() {
   // code to switch avatar animations
-  if( key === '1') {
-    playerAvatar.addMovingAnimation( 'assets/run1.png', 'assets/run2.png');
-    playerAvatar.addStandingAnimation('assets/standing1.png', 'assets/standing2.png');
+  if (key === '1') {
+    playerAvatar.addMovingAnimation('assets/female_running01.png', 'assets/female_running03.png');
+    playerAvatar.addStandingAnimation('assets/female_standing01.png', 'assets/female_standing02.png');
   }
-  if( key === '2') {
-    playerAvatar.addMovingAnimation( 'assets/blob01.png', 'assets/blob08.png');
-    playerAvatar.addStandingAnimation('assets/blob01.png', 'assets/blob08.png');
-  }
-  if( key === '3') {
-    playerAvatar.addMovingAnimation( 'assets/sun1.png', 'assets/sun5.png');
-    playerAvatar.addStandingAnimation('assets/sun1.png', 'assets/sun5.png');
+  if (key === '2') {
+    playerAvatar.addMovingAnimation('assets/male_running01.png', 'assets/male_running03.png');
+    playerAvatar.addStandingAnimation('assets/male_standing01.png', 'assets/male_standing02.png');
   }
 }
 
@@ -152,53 +153,53 @@ function checkMovement() {
   var ySpeed = 0;
 
   // Check x movement
-  if(keyIsDown(RIGHT_ARROW) || keyIsDown(D_KEY)) {
+  if (keyIsDown(RIGHT_ARROW) || keyIsDown(D_KEY)) {
     xSpeed = speed;
   }
-  else if(keyIsDown(LEFT_ARROW) || keyIsDown(A_KEY)) {
+  else if (keyIsDown(LEFT_ARROW) || keyIsDown(A_KEY)) {
     xSpeed = -speed;
   }
-  
+
   // Check y movement
-  if(keyIsDown(DOWN_ARROW) || keyIsDown(S_KEY)) {
+  if (keyIsDown(DOWN_ARROW) || keyIsDown(S_KEY)) {
     ySpeed = speed;
   }
-  else if(keyIsDown(UP_ARROW) || keyIsDown(W_KEY)) {
+  else if (keyIsDown(UP_ARROW) || keyIsDown(W_KEY)) {
     ySpeed = -speed;
   }
 
-  playerAvatar.setSpeed(xSpeed,ySpeed);
+  playerAvatar.setSpeed(xSpeed, ySpeed);
 }
 
 // When the player avatar intesects with the door, gets called repeatedly
 function doorCollision(spriteA, spriteB) {
-  if( playerAvatar.getGrabbableName() === "Key" ) {
+  if (playerAvatar.getGrabbableName() === "Key") {
     opened = true;
   }
   else {
-    playerAvatar.setPosition(width/2, height/2);
+    playerAvatar.setPosition(width / 2, height / 2);
     opened = false;
   }
 }
 
 // When the player avatar intesects with the door, gets called repeatedly
 // SpriteB = grabble sprite
-function grabbableCollision(spriteA, spriteB) {  
+function grabbableCollision(spriteA, spriteB) {
   overlapCount++;
 
-  if( preventPickup || preventRepickup ) {
+  if (preventPickup || preventRepickup) {
     return;
   }
 
   // check for new grabble (not self)
-  if( playerAvatar.grabbable === undefined || playerAvatar.grabbable.sprite !== spriteB ) {
-    for( let i = 0; i < grabbables.length; i++ ) {
-      if( grabbables[i].sprite === spriteB ) {
+  if (playerAvatar.grabbable === undefined || playerAvatar.grabbable.sprite !== spriteB) {
+    for (let i = 0; i < grabbables.length; i++) {
+      if (grabbables[i].sprite === spriteB) {
         //console.log("new set: " + i);
         playerAvatar.setGrabbable(grabbables[i]);
         preventRepickup = true;
         break;
       }
-    }    
+    }
   }
 }
